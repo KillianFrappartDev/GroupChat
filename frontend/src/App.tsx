112ed4b23f11427b65c742ce53b425ce8bd1b249
KillianFrappartDev <killianFrappartDev@Gmail.com>
2020-12-17 16:46:18 +0100

    TypeScript support

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function App() {
   return <h1>Hello World!</h1>;
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Local Imports
+// Local imports
 import App from './App';
 
 ReactDOM.render(
@@ -10,3 +10,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
