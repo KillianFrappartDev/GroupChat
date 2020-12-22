e0eb4eb439001264bff78993637d502eaf08dc3c
KillianFrappartDev <killianFrappartDev@Gmail.com>
2020-12-22 10:46:10 +0100

    Auth Navigation

--- frontend/src/components/Auth/Welcome/index.tsx
+++ frontend/src/components/Auth/Welcome/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Local Imports
 import logo from '../../../assets/cropped.png';
@@ -10,8 +11,12 @@ const Welcome: React.FC<Props> = props => {
   return (
     <div className={styles.container}>
       <img className={styles.logo} alt="GroupChat Logo" src={logo} />
-      <button className={styles.login}>Login</button>
-      <button className={styles.signup}>Signup</button>
+      <Link to="/login">
+        <button className={styles.login}>Login</button>
+      </Link>
+      <Link to="/signup">
+        <button className={styles.signup}>Signup</button>
+      </Link>
       <p className={styles.guest}>Continue as guest</p>
     </div>
   );
--- frontend/src/views/AuthView/index.tsx
+++ frontend/src/views/AuthView/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 // Local Imports
 import Welcome from '../../components/Auth/Welcome/index';
+import Login from '../../components/Auth/Login/index';
+import Signup from '../../components/Auth/Signup/index';
 import styles from './styles.module.scss';
 
 type Props = {};
 
 const AuthView: React.FC<Props> = props => {
-  return <Welcome />;
+  return (
+    <Router>
+      <Route path="/" exact component={Welcome} />
+      <Route path="/login" exact component={Login} />
+      <Route path="/signup" exact component={Signup} />
+    </Router>
+  );
 };
 
 export default AuthView;
