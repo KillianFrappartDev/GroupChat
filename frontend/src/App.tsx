14cb94c3e5202d3a79a8ef9def1c63c4c5db039c
KillianFrappartDev <killianFrappartDev@Gmail.com>
2020-12-22 14:56:29 +0100

    Layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,15 @@ import React from 'react';
 // Local Imports
 import AppView from './views/AppView/index';
 import AuthView from './views/AuthView/index';
+import Modal from './components/Shared/Modal/index';
 
 const App: React.FC = () => {
-  return <AuthView />;
+  return (
+    <>
+      <AppView />
+      <Modal backClick={() => console.log('Clicked')} />
+    </>
+  );
 };
 
 export default App;
--- frontend/src/components/Shared/Modal/index.tsx
+++ frontend/src/components/Shared/Modal/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Local Imports
 import styles from './styles.module.scss';
 
-type Props = {};
+type Props = {
+  backClick: () => void;
+};
 
 const Modal: React.FC<Props> = props => {
-  return <h1>Modal</h1>;
+  return (
+    <div className={styles.backdrop} onClick={props.backClick}>
+      <div className={styles.modal}>
+        <h2>New channel</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
