460d875bea711644b9f5cee77bd4e9d1d4eff918
KillianFrappartDev <killianFrappartDev@Gmail.com>
2020-12-22 11:41:40 +0100

    Skeleton

--- frontend/src/components/Auth/Login/index.tsx
+++ frontend/src/components/Auth/Login/index.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
+import { TextField, FormControlLabel, Checkbox } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 // Local Imports
+import logo from '../../../assets/gc-logo-symbol-nobg.png';
 import styles from './styles.module.scss';
 
 type Props = {};
 
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    background: {
+      default: '#252329'
+    },
+    secondary: {
+      main: '#120F13'
+    }
+  }
+});
+
 const Login: React.FC<Props> = props => {
-  return <h1>Login</h1>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <div className={styles.container}>
+        <img className={styles.logo} alt="logo" src={logo} />
+        <form className={styles.form}>
+          <TextField className={styles.input} id="email" label="Email" variant="outlined" />
+          <TextField className={styles.input} id="password" label="Password" variant="outlined" />
+          <FormControlLabel
+            className={styles.check}
+            control={<Checkbox checked={false} onChange={() => console.log('Click')} name="checkedB" color="primary" />}
+            label="Remember me"
+          />
+        </form>
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default Login;
