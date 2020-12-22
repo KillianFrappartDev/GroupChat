0e00584c6b7dc38ad157f553584e58b562a98153
KillianFrappartDev <killianFrappartDev@Gmail.com>
2020-12-22 16:03:54 +0100

    Setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,9 @@ import React from 'react';
 // Local Imports
 import AppView from './views/AppView/index';
 import AuthView from './views/AuthView/index';
-import Modal from './components/Shared/Modal/index';
 
 const App: React.FC = () => {
-  return <Modal backClick={() => console.log('Clicked')} />;
+  return <AuthView />;
 };
 
 export default App;
