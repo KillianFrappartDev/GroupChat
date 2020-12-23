c14bc88fe9851c33f6cf2b94cdeb99de2c45bf9a
KillianFrappartDev <killianFrappartDev@Gmail.com>
2020-12-23 13:09:56 +0100

    Hashed password

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,11 +1,26 @@
+const bcrypt = require('bcryptjs');
+
+// Local Imports
+const User = require('../models/user');
+
+const findUserWithEmail = async email => {
+  let user;
+  try {
+    user = await User.findOne({ email });
+  } catch (error) {
+    return next(new Error('[ERROR][USERS] Could not find user with email'));
+  }
+  return user;
+};
+
 const login = async (req, res, next) => {
   const { checked, email, password } = req.body;
 
   // Find User with email
+  const user = findUserWithEmail(email);
 
-  // Decrypt password
-
-  // Check if password is valid
+  // Decrypt password & Check if password is valid
+  const decryptedPassword = await bcrypt.compare(password, user.password);
 
   // If Checked is true, create token
 
@@ -15,7 +30,20 @@ const login = async (req, res, next) => {
 };
 
 const signup = async (req, res, next) => {
-  console.log(req.body);
+  const { checked, email, password, username } = req.body;
+
+  // Check if user with this email already exists
+  const existingUser = findUserWithEmail(email);
+
+  // Encrypt password
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  // Create new user
+
+  // If Checked is true, create token
+
+  // Send response
+
   res.json({ message: 'signup' });
 };
 
