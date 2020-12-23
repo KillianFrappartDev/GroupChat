9384ecfbe275580f029c74295d1b21cecb6d9920
KillianFrappartDev <killianFrappartDev@Gmail.com>
2020-12-23 17:03:02 +0100

    Modal Fr

--- frontend/src/components/Shared/Modal/index.tsx
+++ frontend/src/components/Shared/Modal/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField, FormControlLabel, Checkbox } from '@material-ui/core';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
@@ -14,16 +14,55 @@ const darkTheme = createMuiTheme({
 
 type Props = {
   backClick: () => void;
+  onCreate: (title: string, description: string) => void;
 };
 
 const Modal: React.FC<Props> = props => {
+  const [isValid, setIsValid] = useState(true);
+  const [title, setTitle] = useState('');
+  const [titleError, setTitleEror] = useState(false);
+  const [titleHelper, setTitleHelper] = useState('');
+  const [description, setDescription] = useState('');
+
+  const createHandler = (title: string, description: string) => {
+    if (titleError) {
+      setIsValid(false);
+      return;
+    }
+
+    props.onCreate(title, description);
+  };
+
+  const titleHandler = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    if (e.target.value.length <= 0) {
+      setTitleEror(true);
+      setTitleHelper('Title can not be empty.');
+    } else {
+      setTitleEror(false);
+      setTitleHelper('');
+      setIsValid(true);
+    }
+
+    setTitle(e.target.value);
+  };
+
   return (
-    <div className={styles.backdrop} onClick={props.backClick}>
+    <>
+      <div className={styles.backdrop} onClick={props.backClick}></div>
       <div className={styles.modal}>
         <h2>New channel</h2>
         <ThemeProvider theme={darkTheme}>
-          <form className={styles.form}>
-            <TextField className={styles.input} id="title" label="Title" variant="outlined" />
+          <form className={styles.form} onSubmit={e => e.preventDefault()}>
+            <TextField
+              className={styles.input}
+              id="title"
+              label="Title"
+              variant="outlined"
+              onChange={e => titleHandler(e)}
+              helperText={titleHelper}
+              error={titleError}
+              value={title}
+            />
             <TextField
               className={styles.input}
               id="description"
@@ -31,12 +70,14 @@ const Modal: React.FC<Props> = props => {
               label="Description"
               variant="outlined"
               multiline
+              value={description}
+              onChange={e => setDescription(e.target.value)}
             />
-            <CustomButton onClick={() => console.log('Clicked')} isPurple title="Create" small />
+            <CustomButton onClick={() => createHandler(title, description)} isPurple title="Create" small />
           </form>
         </ThemeProvider>
       </div>
-    </div>
+    </>
   );
 };
 
--- frontend/src/views/AppView/index.tsx
+++ frontend/src/views/AppView/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 // Local Imports
 import Messages from '../../components/Main/Messages/index';
@@ -10,13 +11,27 @@ import Search from '../../components/Side/Search/index';
 import Groups from '../../components/Side/Groups/index';
 import GroupInfo from '../../components/Side/GroupInfo/index';
 import Members from '../../components/Side/Members/index';
+import Modal from '../../components/Shared/Modal/index';
 import { DUMMY_MESSAGES, DUMMY_GROUPS, DUMMY_MEMBERS } from '../../utils/dummy-data';
 import styles from './styles.module.scss';
 
 type Props = {};
 
 const AppView: React.FC<Props> = props => {
+  const dispatch = useDispatch();
   const [inChannel, setInChannel] = useState(true);
+  const [modal, setModal] = useState(false);
+
+  // Async Requests
+  const createGroup = async (title: string, description: string) => {
+    console.log(title, description);
+    setModal(false);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('userData');
+    dispatch({ type: 'LOGOUT' });
+  };
 
   let sideContent;
   if (inChannel) {
@@ -38,15 +53,16 @@ const AppView: React.FC<Props> = props => {
   return (
     <div className={styles.container}>
       <div className={styles.side}>
-        <SideTopBar inChannel arrowClick={() => setInChannel(false)} plusClick={() => console.log('Clicked')} />
+        <SideTopBar inChannel={inChannel} arrowClick={() => setInChannel(false)} plusClick={() => setModal(true)} />
         {sideContent}
-        <BottomBar exitClick={() => console.log('Clicked')} />
+        <BottomBar exitClick={logoutHandler} />
       </div>
       <div className={styles.main}>
         <MainTopBar title="Welcome 👋" menuClick={() => console.log('Clicked')} />
         <Messages messages={DUMMY_MESSAGES} />
         <MsgInput />
       </div>
+      {modal && <Modal backClick={() => setModal(false)} onCreate={createGroup} />}
     </div>
   );
 };
