0d920c6b07ef35199b3fba03609f0f9a670b94be
KillianFrappartDev <killianFrappartDev@Gmail.com>
2021-01-02 12:17:03 +0100

    Backend setup

---
+++ backend/controllers/bugs-controllers.js
@@ -0,0 +1,9 @@
+// Local Imports
+const Bug = require('../models/bug');
+
+const fetch = async (req, res, next) => {};
+
+const report = async (req, res, next) => {};
+
+exports.fetch = fetch;
+exports.report = report;
