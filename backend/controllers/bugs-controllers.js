cf2d4a3ee62fd39a0cf85315824da7f9fa0b7746
KillianFrappartDev <killianFrappartDev@Gmail.com>
2021-01-02 12:53:58 +0100

    Report bug feature works

--- backend/controllers/bugs-controllers.js
+++ backend/controllers/bugs-controllers.js
@@ -1,9 +1,35 @@
 // Local Imports
 const Bug = require('../models/bug');
+const User = require('../models/user');
 
 const fetch = async (req, res, next) => {};
 
-const report = async (req, res, next) => {};
+const report = async (req, res, next) => {
+  const { id, title, description } = req.body;
+
+  // Find user
+  let user;
+  try {
+    user = await User.findById(id);
+  } catch (error) {
+    return next(new Error('[ERROR][BUGS] Could not find user by id: ' + error));
+  }
+
+  // Create a bug
+  const bug = new Bug({ title, description, user });
+
+  // Save bug
+  try {
+    await bug.save();
+  } catch (error) {
+    return next(new Error('[ERROR][BUGS] Could not create bug: ' + error));
+  }
+
+  // Send response
+  res.json({
+    message: '[BUGS][CREATE] Bug reported..'
+  });
+};
 
 exports.fetch = fetch;
 exports.report = report;
--- backend/models/bug.js
+++ backend/models/bug.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const schema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  members: { type: mongoose.Types.ObjectId, required: true, ref: 'User' }
+  user: { type: mongoose.Types.ObjectId, required: true, ref: 'User' }
 });
 
 module.exports = mongoose.model('Bug', schema);
