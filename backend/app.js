e07a5bf4f5971283e1318f40cd64bd4514322bf0
KillianFrappartDev <killianFrappartDev@Gmail.com>
2021-01-02 13:33:23 +0100

    Merge done

--- backend/app.js
+++ backend/app.js
@@ -25,11 +25,59 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || 'An unknown error occured.', error: true });
 });
 
+// Socket.io
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+class User {
+  constructor(uid, sid) {
+    this.uid = uid;
+    this.sid = sid;
+    this.gid;
+  }
+}
+
+let userList = [];
+
+// Establish a connection
+io.on('connection', socket => {
+  // New user
+  socket.on('new user', uid => {
+    userList.push(new User(uid, socket.id));
+  });
+
+  // Join group
+  socket.on('join group', (uid, gid) => {
+    for (let i = 0; i < userList.length; i++) {
+      if (socket.id === userList[i].sid) userList[i].gid = gid;
+    }
+  });
+
+  // New group
+  socket.on('create group', (uid, title) => {
+    io.emit('fetch group');
+  });
+
+  // New message
+  socket.on('message', (uid, gid) => {
+    for (const user of userList) {
+      if (gid === user.gid) io.to(user.sid).emit('fetch messages', gid);
+    }
+  });
+
+  // Close connection
+  socket.on('disconnect', () => {
+    for (let i = 0; i < userList.length; i++) {
+      if (socket.id === userList[i].sid) userList.splice(i, 1);
+    }
+  });
+});
+
 // Connect to DB && Start server
 mongoose
   .connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => {
-    app.listen(process.env.PORT || 5000, () =>
+    server.listen(process.env.PORT || 5000, () =>
       console.log(`Server up and running on port ${process.env.PORT || 5000}!`)
     );
   })
