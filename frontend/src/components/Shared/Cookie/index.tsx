710091abd927ba8a7d9283e8bc95edda21027aa1
KillianFrappartDev <killianFrappartDev@Gmail.com>
2021-01-11 15:33:09 +0100

    Design & responsive

--- frontend/src/components/Shared/Cookie/index.tsx
+++ frontend/src/components/Shared/Cookie/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 // Local Imports
 import image from '../../../assets/cookies.png';
+import CustomButton from '../CustomButton/index';
 import styles from './styles.module.scss';
 
 type Props = {};
@@ -10,7 +11,17 @@ const Cookie: React.FC<Props> = props => {
   const [open, setOpen] = useState(true);
   return (
     <div className={styles.container}>
-      {open && <div className={styles.box}></div>}
+      {open && (
+        <div className={styles.box}>
+          <h3 className={styles.title}>Are you hungry ?</h3>
+          <div className={styles.actions}>
+            <CustomButton isPurple small title="Accept cookies" onClick={() => {}} />
+            <a className={styles.info} target="_blank" href="https://ec.europa.eu/info/cookies_en">
+              More information.
+            </a>
+          </div>
+        </div>
+      )}
       <img className={styles.cookie} onClick={() => setOpen(prev => !prev)} src={image} />
     </div>
   );
