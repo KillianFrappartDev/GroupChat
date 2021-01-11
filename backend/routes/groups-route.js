fe1811b3f4d92e4ab5976ca2ce17db39bc95799f
KillianFrappartDev <killianFrappartDev@Gmail.com>
2021-01-11 13:45:23 +0100

    Back validation

--- backend/routes/groups-route.js
+++ backend/routes/groups-route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 // Local Imports
 const controllers = require('../controllers/groups-controllers');
@@ -7,6 +8,6 @@ const router = express.Router();
 
 router.get('/:gid', controllers.fetchGroupData);
 router.get('/', controllers.fetchGroups);
-router.post('/', controllers.createGroup);
+router.post('/', body('title').isLength({ min: 3, max: 12 }), controllers.createGroup);
 
 module.exports = router;
--- backend/routes/users-route.js
+++ backend/routes/users-route.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 // Local Imports
 const controllers = require('../controllers/users-controllers');
 
 const router = express.Router();
 
-router.post('/login', controllers.login);
-router.post('/signup', controllers.signup);
-router.put('/edit', controllers.edit);
+router.post('/login', body('email').isEmail(), body('password').isLength({ min: 6, max: 20 }), controllers.login);
+router.post(
+  '/signup',
+  body('email').isEmail(),
+  body('password').isLength({ min: 6, max: 20 }),
+  body('username').isLength({ min: 3, max: 12 }),
+  controllers.signup
+);
+router.put('/edit', body('username').isLength({ min: 3, max: 12 }), controllers.edit);
 router.post('/guest', controllers.guest);
 router.post('/verify', controllers.verify);
 
